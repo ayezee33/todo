7efaad6b6c3b6f55857e3b6eb899e606489b2a6a
ayezee33 <ayezee33@gmail.com>
2016-11-23 07:39:21 -0800

    Add icon and and sweet alerts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,34 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap.min
+//= require sweetalert2
 //= require_tree .
+
+//Override the default confirm dialog by rails
+$.rails.allowAction = function(link){
+  if (link.data("confirm") == undefined){
+    return true;
+  }
+  $.rails.showConfirmationDialog(link);
+  return false;
+}
+
+//User click confirm button
+$.rails.confirmed = function(link){
+  link.data("confirm", null);
+  link.trigger("click.rails");
+}
+
+//Display the confirmation dialog
+$.rails.showConfirmationDialog = function(link){
+  var message = link.data("confirm");
+  swal({
+    title: message,
+    type: 'warning',
+    confirmButtonText: 'Yes, delete',
+    confirmButtonColor: '#234567',
+    showCancelButton: true
+  }).then(function(e){
+    $.rails.confirmed(link);
+  });
+};
